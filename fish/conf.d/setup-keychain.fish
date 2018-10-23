35dcbabf1deace920872e58a682012057b899a48
Vanshidhar Konda <vanshidhar.r.konda@intel.com>
2018-10-23 15:57:32 -0700

    Remove proxy and keychain setup from repository

--- fish/conf.d/setup-keychain.fish
+++
@@ -1,3 +0,0 @@
-# Setup keychain for all fish shells
-eval (keychain --eval --agents ssh --quiet ~/.ssh/fxr-simics-key/id_rsa)
-eval (keychain --eval --agents ssh --quiet ~/.ssh/vrkonda-desk-key/id_rsa)
--- fish/conf.d/setup-proxy.fish
+++
@@ -1,6 +0,0 @@
-# Setup proxy variables
-set -Ux http_proxy http://proxy-us.intel.com:911
-set -Ux https_proxy http://proxy-us.intel.com:911
-set -Ux ftp_proxy http://proxy-us.intel.com:911
-set -Ux no_proxy intel.com
-
