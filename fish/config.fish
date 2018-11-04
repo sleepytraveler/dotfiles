1dd9bd712112f9336d94e4b93336accc548c0ee0
Vanshidhar Konda <vrkonda@Watson.mshome.net>
2018-11-04 01:09:26 -0700

    Move powerline from Python 2.7 to 3.7

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,7 @@ end
 
 
 # Setup powerline for fish
-set fish_powerline_path "$HOME/.local/lib/python2.7/site-packages/powerline/bindings/fish"
+set fish_powerline_path "$HOME/.local/lib/python3.7/site-packages/powerline/bindings/fish"
 set fish_function_path $fish_function_path "$HOME/.local/bin"
 source "$fish_powerline_path/powerline-setup.fish"
 powerline-setup
