1e0ee9ce79fb98e24d72d4874026d6812a098550
Vanshidhar Konda <kvanshidhar@live.com>
2018-12-18 16:23:53 -0800

    Cleanup path for fish and bash shells

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,15 @@
 # Setup PATH for user
-if test -d "$HOME/.local/bin"
-	set PATH $PATH "$HOME/.local/bin"
-end
+#if test -d "$HOME/.local/bin"
+	#set PATH $PATH "$HOME/.local/bin"
+#end
 
-if test -d "$HOME/bin"
-	set PATH "$HOME/bin"
-end
+#if test -d "$HOME/bin"
+	#set PATH "$HOME/bin"
+#end
 
 
 # Setup powerline for fish
-set fish_powerline_path "$HOME/.local/lib/python3.7/site-packages/powerline/bindings/fish"
-set fish_function_path $fish_function_path "$HOME/.local/bin"
-source "$fish_powerline_path/powerline-setup.fish"
+set fish_function_path $fish_function_path "$HOME/.local/lib/python3.7/site-packages/powerline/bindings/fish"
 powerline-setup
 
 # Set the EDITOR variable
