c5062e77810b2b2cffc9c4ac29b4e29c96882ca1
Vanshidhar Konda <kvanshidhar@live.com>
2018-12-29 13:44:41 -0800

    Fix fish path

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,7 @@
 # Setup PATH for user
-#if test -d "$HOME/.local/bin"
-	#set PATH $PATH "$HOME/.local/bin"
-#end
-
-#if test -d "$HOME/bin"
-	#set PATH "$HOME/bin"
-#end
-
+if test -d "$HOME/.local/bin"
+	set PATH $PATH "$HOME/.local/bin"
+end
 
 # Setup powerline for fish
 set fish_function_path $fish_function_path "$HOME/.local/lib/python3.7/site-packages/powerline/bindings/fish"
