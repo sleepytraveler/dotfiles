356e7a90bfce3254babebc1726ea11ea84aae1a6
Vanshidhar Konda <kvanshidhar@live.com>
2019-02-12 08:42:21 -0800

    Remove adding .local/bin to the path for Fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,3 @@
-# Setup PATH for user
-if test -d "$HOME/.local/bin"
-	set PATH $PATH "$HOME/.local/bin"
-end
-
 # Setup powerline for fish
 set fish_function_path $fish_function_path "$HOME/.config/fish/powerline/bindings/fish"
 powerline-setup
