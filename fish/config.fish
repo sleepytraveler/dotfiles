c2e24d595b4ed2c0a0696b5f12830629344b9545
Vanshidhar Konda <kvanshidhar@live.com>
2019-06-20 11:35:44 -0700

    Fish: Switch to pure theme from powerline

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 # Setup powerline for fish
-set fish_function_path $fish_function_path "$HOME/.config/fish/powerline/bindings/fish"
-powerline-setup
+# set fish_function_path $fish_function_path "$HOME/.config/fish/powerline/bindings/fish"
+# powerline-setup
 
 # Set the EDITOR variable
 set -Ux EDITOR nvim
+# THEME PURE #
+set fish_function_path /home/vrkonda/.config/fish/functions/theme-pure/functions/ $fish_function_path
+source /home/vrkonda/.config/fish/functions/theme-pure/conf.d/pure.fish
