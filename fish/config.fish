a5ed01ab1f8df8ec0f435683680bf599ffb77d69
Vanshidhar Konda <kvanshidhar@live.com>
2020-01-22 08:19:49 -0800

    Fish: Add cargo/rust to the PATH variable

--- fish/config.fish
+++ fish/config.fish
@@ -8,5 +8,5 @@ set -Ux EDITOR nvim
 set fish_function_path $HOME/.config/fish/functions/theme-pure/functions/ $fish_function_path
 source $HOME/.config/fish/functions/theme-pure/conf.d/pure.fish
 
-# Add ~/.local/bin to PATH
-set PATH $PATH $HOME/.local/bin/
+set PATH  $HOME/.local/bin/ $PATH
+set PATH  $HOME/.cargo/bin/ $PATH
