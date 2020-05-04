3de973f0092c366f719272588932d42a56e47b97
Vanshidhar Konda <kvanshidhar@live.com>
2020-05-04 16:43:05 -0700

    fish: Remove trailing / when adding to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -9,11 +9,11 @@ set fish_function_path $HOME/.config/fish/functions/theme-pure/functions/ $fish_
 source $HOME/.config/fish/functions/theme-pure/conf.d/pure.fish
 
 if test -d $HOME/.local/bin
-	set PATH $HOME/.local/bin/ $PATH
+	set PATH $HOME/.local/bin $PATH
 end
 
 if test -d $HOME/.cargo/bin
-	set PATH $HOME/.cargo/bin/ $PATH
+	set PATH $HOME/.cargo/bin $PATH
 end
 
 if test -d $HOME/yarn/bin
