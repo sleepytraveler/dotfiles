40234d4e7d870a7bde77ef813eba17b7d09b86b6
Vanshidhar Konda <kvanshidhar@live.com>
2020-05-27 16:23:09 -0700

    Mutlikey ssh: Add keys only if they exist

--- autostart-scripts/ssh-add.sh
+++ autostart-scripts/ssh-add.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ -f "$HOME/.ssh/github/sleepytraveler" ]
+if [ -f "$HOME/.ssh/github/sleepytraveler" ]; then
 	ssh-add -q ~/.ssh/github/sleepytraveler < /dev/null
 fi
 
-if [ -f "$HOME/.ssh/gitlab/vkonda_ampere" ]
+if [ -f "$HOME/.ssh/gitlab/vkonda_ampere" ]; then
 	ssh-add -q ~/.ssh/gitlab/vkonda_ampere < /dev/null
 fi
 
-if [ -f "$HOME/.ssh/ampere/vkonda" ]
+if [ -f "$HOME/.ssh/ampere/vkonda" ]; then
 	ssh-add -q ~/.ssh/ampere/vkonda < /dev/null
 fi
